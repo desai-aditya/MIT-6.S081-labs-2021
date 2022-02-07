14afa5ee5b4edb1b3acc8be9eb10496a3ba813a2
desai-aditya <desaiadityapr@gmail.com>
2022-02-07 08:43:28 -0800

    solution for pgtbl lab - print pte task

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,34 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// Recursively walk and print page-table pages.
+void
+_vmprinthelper(pagetable_t pagetable,int times )
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+	if(pte & PTE_V)
+	{
+		for(int i = 0 ; i < times; i++)
+			printf(" ..");
+    	printf("%d: pte %p pa %p\n",i,pte,PTE2PA(pte));
+	}
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      _vmprinthelper((pagetable_t)child,times+1);
+    } 
+  }
+}
+
+// for the pgtbl lab - print pte task
+// Function to print a pagetable and all its entries in the tree by walking through it
+void
+vmprint(pagetable_t pagetable)
+{
+	printf("page table %p\n",pagetable);
+	_vmprinthelper(pagetable,1);
+}
+
